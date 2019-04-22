9d0c3167b3382a288bd69f0fb4375cdada7ede6a
RoryStaehelin <45770848+RoryStaehelin@users.noreply.github.com>
2019-04-22 04:18:53 +0100

    This version prints information on whats going on to a console

---
+++ RLGYG.py
@@ -0,0 +1,40 @@
+from time import sleep
+import os
+
+def isrunning(name):
+    processlist = os.popen('tasklist').readlines()
+    for process in processlist:
+        if name == process[:len(name)]:
+            return True
+    return False
+
+rlEXE = "RocketLeague.exe"
+gygEXE = "Gif Your Game.exe"
+gygPATH = "C:\\Users\\Rory\\AppData\\Local\\Programs\\badpanda-react\\Gif Your Game.exe"
+GYG = False
+print("Program running")
+sleep(1)
+
+if isrunning(gygEXE):
+    GYG = True
+    print("Gif Your Game is running...")
+
+while True:
+    while not GYG:
+        if isrunning(rlEXE):
+            print("Rocket League is running...")
+            print("Starting Gif Your Game")
+            os.startfile(gygPATH, "open")
+            sleep(3)
+            os.system("cls")
+            print("Gif Your Game launched successfully!")
+            GYG = True
+        sleep(20)
+        print("Checking...")
+    while GYG:
+        if not isrunning(rlEXE):
+            os.system("taskkill /f /im  \"Gif Your Game.exe\"")
+            print("Closing Gif Your Game...")
+            GYG = False
+        sleep(90)
+        print("Checking...")
